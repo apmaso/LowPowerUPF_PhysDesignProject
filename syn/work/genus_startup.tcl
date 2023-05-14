43db09324e4ad1eb6b554af705f7fdf5792c1ff0
Alexander Maso <amaso@mo.ece.pdx.edu>
2023-05-14 15:52:10 -0700

    Made alias lp to set the top design as mv_lp_top and ran synth through genus but no schematic was created...  I imagine I am missing something here but will ask tomorrow evening before/after lecture

--- syn/work/genus_startup.tcl
+++ syn/work/genus_startup.tcl
@@ -3,6 +3,10 @@ alias fs "set top_design fifo1_sram"
 alias f "set top_design fifo1"
 alias o "set top_design ORCA_TOP"
 alias e "set top_design ExampleRocketSystem"
+
+# Adding my own alias for mv_lp_top... what is simple_and...? (and should I use it)
+alias lp "set top_design mv_lp_top"
+
 set_db timing_report_fields "delay arrival cell timing_point"
 history keep 100
 set systemTime [clock seconds]
